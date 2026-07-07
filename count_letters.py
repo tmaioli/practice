d629e493c0dc6a7dd8b17a89c61b8de32186a0f7
Thomas Maioli <47154068+tmaioli@users.noreply.github.com>
2026-07-07 02:18:53 -0400

    Revise project details and add output example
    Updated project metadata and added example output.

--- count_letters.py
+++ count_letters.py
@@ -1,3 +1,4 @@
+"""
 # Project Title: Count Letters
 # Version: 1.0
 # Instructions:
@@ -5,16 +6,21 @@
 # Built with: Atom text editor
 # Thomas Maioli
 # coded in: Python 3.7
-# date: 2019-02-20
+# date: 2019-02-20 and 2026-07-07
 # Project name: count letters
 # Issues and concerns: none
 # Problem domain: length and count
-# Keep your code dry. :)
-
-
-# for letters: "e" and "a" in random_tip
 # [ ] print letter counts
 # [ ] BONUS: print which letter is most frequent
+
+Enter a sentence: the cat ran fast
+Your String is this many charaters 16
+Here are your letter counts
+Counter({'t': 3, ' ': 3, 'a': 3, 'h': 1, 'e': 1, 'c': 1, 'r': 1, 'n': 1, 'f': 1, 's': 1})
+
+=== Code Execution Successful ===
+
+"""
 
 random_tip = "none"
 
